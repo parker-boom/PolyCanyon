daf49c560abdbf87d345f8680873b64ce36d4cb1
Parker Jones <pjones15@calpoly.edu>
2024-07-19 14:00:58 -0700

    PopUp works but location glitch infintely reloads

--- react/src/MapView.js
+++ react/src/MapView.js
@@ -5,6 +5,7 @@ import { requestLocationPermission, getCurrentLocation, isWithinSafeZone } from 
 import Geolocation from '@react-native-community/geolocation';
 import StructPopUp from './StructPopUp';
 import { useStructures } from './StructureData';
+import { useMapPoints } from './MapPoint';
 
 const PulsingCircle = ({ isSatelliteView }) => {
     const scaleAnim = useRef(new Animated.Value(1)).current;
@@ -61,36 +62,36 @@ const VisitedStructurePopup = ({ structure, isPresented, setIsPresented, isDarkM
 
     return (
         <View style={styles.popupContainer}>
-            <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
-                <Icon 
-                    name="close"
-                    size={28}
-                    color={isDarkMode ? 'white' : 'black'}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.contentContainer} onPress={handleStructurePress}>
+            <View style={styles.contentContainer}>
+                <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
+                    <Icon 
+                        name="close"
+                        size={28}
+                        color={isDarkMode ? 'white' : 'black'}
+                    />
+                </TouchableOpacity>
                 <Image
                     source={structure.closeUpImage}
                     style={styles.popupImage}
                 />
-                <View style={styles.textContainer}>
+                <TouchableOpacity style={styles.textContainer} onPress={handleStructurePress}>
                     <Text style={[styles.justVisitedText, { color: isDarkMode ? 'rgba(255,255,255,0.6)' : 'rgba(0,0,0,0.8)' }]}>
                         Just Visited!
                     </Text>
                     <Text style={[styles.titleText, { color: isDarkMode ? 'white' : 'black' }]}>
                         {structure.title}
                     </Text>
-                </View>
-                <Text style={[styles.numberText, { color: isDarkMode ? 'rgba(255,255,255,0.7)' : 'rgba(0,0,0,0.7)' }]}>
-                    {structure.number}
-                </Text>
+                    <Text style={[styles.numberText, { color: isDarkMode ? 'rgba(255,255,255,0.7)' : 'rgba(0,0,0,0.7)' }]}>
+                        {structure.number}
+                    </Text>
+                </TouchableOpacity>
                 <Icon
                     name="chevron-forward"
                     size={20}
                     color={isDarkMode ? 'white' : 'black'}
                     style={styles.chevron}
                 />
-            </TouchableOpacity>
+            </View>
             {showStructPopup && (
                 <StructPopUp
                     structure={structure}
@@ -106,7 +107,7 @@ const VisitedStructurePopup = ({ structure, isPresented, setIsPresented, isDarkM
 };
 
 const MapView = ({ route }) => {
-    const { mapPoints } = route.params;
+    const { mapPoints } = useMapPoints();
     const { structures, setStructures } = useStructures();
     const [isDarkMode, setIsDarkMode] = useState(false);
     const [isSatelliteView, setIsSatelliteView] = useState(false);
@@ -125,34 +126,27 @@ const MapView = ({ route }) => {
     const MAP_ORIGINAL_HEIGHT = 4164;
 
     useEffect(() => {
-        console.log('MapView mounted. mapPoints:', mapPoints);
-        console.log('Initial structures:', structures);
-        
         requestLocationPermission();
         const watchId = Geolocation.watchPosition(
             (position) => handleLocationUpdate(position),
-            (error) => console.log('Error watching position:', error),
+            (error) => {},
             { enableHighAccuracy: true, distanceFilter: 10, interval: 5000, fastestInterval: 2000 }
         );
 
         return () => {
             Geolocation.clearWatch(watchId);
         };
-    }, []);
+    }, [mapPoints, structures]);
 
     const handleLocationUpdate = (position) => {
-        console.log('Location updated:', position.coords);
         setLocation(position);
         if (isWithinSafeZone(position.coords)) {
             const nearest = findNearestMapPoint(position.coords, mapPoints);
-            console.log('Nearest point:', nearest);
             setNearestPoint(nearest);
             if (nearest && nearest.landmark !== -1) {
-                console.log('Attempting to mark structure as visited. Landmark:', nearest.landmark);
                 markStructureAsVisited(nearest.landmark);
             }
         } else {
-            console.log('Location is outside safe zone');
             setNearestPoint(null);
         }
     };
@@ -172,28 +166,21 @@ const MapView = ({ route }) => {
             }
         });
 
-        console.log('Finding nearest map point for coordinate:', coordinate);
         return nearest;
     };
 
     const markStructureAsVisited = (landmarkId) => {
-        console.log('Marking structure as visited. LandmarkId:', landmarkId);
         setStructures(prevStructures => {
             const updatedStructures = prevStructures.map(structure => {
                 if (structure.number === landmarkId) {
-                    console.log('Found matching structure:', structure);
                     if (!structure.isVisited) {
-                        console.log('Structure was not previously visited. Marking as visited.');
                         setVisitedStructure(structure);
                         setShowPopup(true);
-                    } else {
-                        console.log('Structure was already visited.');
                     }
                     return { ...structure, isVisited: true };
                 }
                 return structure;
             });
-            console.log('Updated structures:', updatedStructures);
             return updatedStructures;
         });
 
@@ -218,15 +205,11 @@ const MapView = ({ route }) => {
     };
 
     const markPointAsVisitedByIndex = (index) => {
-        console.log('Marking point as visited by index:', index);
         const updatedMapPoints = [...mapPoints];
         if (index >= 0 && index < updatedMapPoints.length) {
             const newIndex = index - 1;
             updatedMapPoints[newIndex].isVisited = true;
-            console.log('Updated map point:', updatedMapPoints[newIndex]);
             markStructureAsVisited(updatedMapPoints[newIndex].landmark);
-        } else {
-            console.log('Invalid index:', index);
         }
     };
 
@@ -333,14 +316,6 @@ const styles = StyleSheet.create({
         shadowRadius: 5,
         elevation: 25,
     },
-    locationText: {
-        position: 'absolute',
-        bottom: 20,
-        left: 20,
-        backgroundColor: 'rgba(255,255,255,0.7)',
-        padding: 10,
-        borderRadius: 5,
-    },
     markerContainer: {
         position: 'absolute',
         width: 20,
@@ -397,12 +372,6 @@ const styles = StyleSheet.create({
         right: 15,
         backgroundColor: 'transparent',
     },
-    closeButton: {
-        position: 'absolute',
-        top: 10,
-        left: 10,
-        zIndex: 1,
-    },
     contentContainer: {
         flexDirection: 'row',
         alignItems: 'center',
@@ -418,10 +387,14 @@ const styles = StyleSheet.create({
         shadowRadius: 3.84,
         elevation: 5,
     },
+    closeButton: {
+        padding: 5,
+    },
     popupImage: {
         width: 80,
         height: 80,
         borderRadius: 10,
+        marginLeft: 10,
     },
     textContainer: {
         flex: 1,
@@ -437,7 +410,6 @@ const styles = StyleSheet.create({
     numberText: {
         fontSize: 28,
         fontWeight: 'bold',
-        marginLeft: 5,
     },
     chevron: {
         marginLeft: 10,
